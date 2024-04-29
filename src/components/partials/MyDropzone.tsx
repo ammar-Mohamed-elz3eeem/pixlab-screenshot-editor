55ac06b4a6d3c2bf69745b6a3614c559d0b9d34c
Ammar-Massoud <ammar@wpkama.com>
2024-04-29 17:52:25 +0300

    add new useFabricImage

--- src/components/partials/MyDropzone.tsx
+++ src/components/partials/MyDropzone.tsx
@@ -2,51 +2,14 @@
 /* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
 import { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
-import useApp from '../../hooks/useContext'
-import useFabric from '../../hooks/useFabric'
-import { fabric } from 'fabric'
+import useFabricImage from '../../hooks/useFabricImage'
 
 export default function MyDropzone(): JSX.Element {
-  const { setImage } = useApp()
-  const {
-    canvas: { setFabImage, fabContext },
-  } = useFabric()
-  const { selectedThumbType } = useApp()
+  const fabricImageInit = useFabricImage()
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     acceptedFiles.forEach((file: File) => {
-      const url = URL.createObjectURL(file)
-      setImage(url)
-      fabric.Image.fromURL(url, (image) => {
-        image.scaleToHeight(selectedThumbType?.height! / 1.5)
-        image.scaleToWidth(selectedThumbType?.width! / 1.5)
-        image.set(
-          'clipPath',
-          new fabric.Rect({
-            width: image.width,
-            height: image.height,
-            rx: 20, // Set the x-radius to 20 for example, adjust as needed
-            ry: 20, // Set the y-radius to 20 for example, adjust as needed
-            originX: 'center',
-            originY: 'center',
-          }),
-        )
-
-        // Add the frame to the canvas
-        fabContext?.add(image).centerObject(image)
-        const frame = new fabric.Rect({
-          width: selectedThumbType?.width! / 1.5 + 20,
-          height: selectedThumbType?.height! / 1.5 + 20,
-          fill: 'white',
-          stroke: 'black',
-          strokeWidth: 20, // Adjust the thickness of the frame as needed
-          originX: 'center',
-          originY: 'center',
-        })
-        fabContext?.add(frame).centerObject(frame)
-        console.log(frame, image)
-        setFabImage(image)
-      })
+      fabricImageInit(file)
     })
   }, [])
 
---
+++ src/hooks/useFabricImage.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+import useApp from './useContext'
+import useFabric from './useFabric'
+import { fabric } from 'fabric'
+
+export default function useFabricImage(): (file: any) => void {
+  const { setImage, selectedThumbType } = useApp()
+  const {
+    canvas: { setFabImage, fabContext },
+  } = useFabric()
+
+  return (file: any) => {
+    const url = URL.createObjectURL(file)
+    setImage(url)
+    fabric.Image.fromURL(url, (image) => {
+      image.scaleToHeight(selectedThumbType?.height! / 1.5)
+      image.scaleToWidth(selectedThumbType?.width! / 1.5)
+      image.set(
+        'clipPath',
+        new fabric.Rect({
+          width: image.width,
+          height: image.height,
+          rx: 20, // Set the x-radius to 20 for example, adjust as needed
+          ry: 20, // Set the y-radius to 20 for example, adjust as needed
+          originX: 'center',
+          originY: 'center',
+        }),
+      )
+
+      // Add the frame to the canvas
+      fabContext?.add(image).centerObject(image)
+      const frame = new fabric.Rect({
+        width: selectedThumbType?.width! / 1.5 + 20,
+        height: selectedThumbType?.height! / 1.5 + 20,
+        fill: 'white',
+        stroke: 'black',
+        strokeWidth: 20, // Adjust the thickness of the frame as needed
+        originX: 'center',
+        originY: 'center',
+      })
+      fabContext?.add(frame).centerObject(frame)
+      console.log(frame, image)
+      setFabImage(image)
+    })
+  }
+}
