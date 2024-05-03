f74a4cb1bd37728a2de73d87869711ed9de1fe96
HAidy2 <haydyalhfny@gmail.com>
2024-05-03 18:02:35 +0300

    Welcome page

--- src/components/WelcomPage.tsx
+++ src/components/WelcomPage.tsx
@@ -42,79 +42,79 @@ const WelcomePage = (): ReactElement => {
         flex-row top-8 flex rounded-2xl 
         overflow-y-scroll'
     >
-
       <div
         style={{
           width: '50%',
-          height:'100%',
+          height: '100%',
           display: 'flex',
           flexDirection: 'column',
           gap: '2rem',
-          justifyContent:'center',
+          justifyContent: 'center',
           alignItems: 'start',
           paddingLeft: '3rem',
         }}
       >
-        <div style={{width:'100%',
-                     display:'flex',
-                     flexDirection:'column',
-                     gap:'0.5rem'
-        }}>
-        <Logo />
-             <div style={{height:'5px',
-             marginBottom:'0.5rem',
-                      width:'100%',
-             backgroundImage:'linear-gradient(to right, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #ff4e50 50%, #ff4e50 62.5%, #8e9eab 62.5%, #8e9eab 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4)',
-                     borderTop: '0',
-                    borderRadius:'5px',}}/>
-
-        </div>
-
-        <div style={{display:'flex',
-                     flexDirection:'column',
-                     gap:'2rem',
-                     justifyContent:'center',
-                     alignItems:'start',
-        }}>
-        <h4 style={{ fontSize: '20px',
-          lineHeight:'30px',
-         }}>
-          Welcome to our revolutionary AI Image Editor, where creativity meets technology to
-          transform your images like never before. Say goodbye to tedious manual editing and hello
-          to effortless, professional-quality results at your fingertips.
-          Ready to take your images to the next level? Sign up for AI Image Editor today and experience the future of image editing.
-           With our powerful AI tools, the possibilities are endless.
-        </h4>
-        <div
-          style={{
-            width: '50%',
-            padding: '1rem',
-            backgroundColor: '#4596fdfc',
-            borderRadius: '40px',
-            textAlign:'center',
-          }}
-          {...getRootProps()}
-        >
-          <label
-            htmlFor='upload-input'
-            id='upload-label'
+        <div style={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+          <Logo />
+          <div
             style={{
-              color: 'white',
-              backgroundColor: '#4596fdfc',
-              fontSize: '27px',
-              lineHeight:'20px',
+              height: '5px',
+              marginBottom: '0.5rem',
+              width: '100%',
+              backgroundImage:
+                'linear-gradient(to right, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #ff4e50 50%, #ff4e50 62.5%, #8e9eab 62.5%, #8e9eab 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4)',
+              borderTop: '0',
+              borderRadius: '5px',
             }}
-          >
-            Load Images
-          </label>
-          <input
-            type='file'
-            id='upload-input'
-            style={{ display: 'none' }}
-            accept='image/*'
-            {...getInputProps()}
           />
         </div>
+
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '2rem',
+            justifyContent: 'center',
+            alignItems: 'start',
+          }}
+        >
+          <h4 style={{ fontSize: '20px', lineHeight: '30px' }}>
+            Welcome to our revolutionary AI Image Editor, where creativity meets technology to
+            transform your images like never before. Say goodbye to tedious manual editing and hello
+            to effortless, professional-quality results at your fingertips. Ready to take your
+            images to the next level? Sign up for AI Image Editor today and experience the future of
+            image editing. With our powerful AI tools, the possibilities are endless.
+          </h4>
+          <div
+            style={{
+              width: '50%',
+              padding: '1rem',
+              backgroundColor: '#4596fdfc',
+              borderRadius: '40px',
+              textAlign: 'center',
+            }}
+            {...getRootProps()}
+          >
+            <label
+              htmlFor='upload-input'
+              id='upload-label'
+              style={{
+                color: 'white',
+                backgroundColor: '#4596fdfc',
+                fontSize: '27px',
+                lineHeight: '20px',
+              }}
+            >
+              Load Images
+            </label>
+            <input
+              type='file'
+              id='upload-input'
+              style={{ display: 'none' }}
+              accept='image/*'
+              {...getInputProps()}
+            />
+          </div>
         </div>
       </div>
       <div style={{ width: '40%', height: '90%' }}>
